6ff8ae46e7c6a55abedf1da242898d6bc533bc62
almax-21 <almaev21.max@gmail.com>
2021-02-06 11:37:51 +0400

    Add overlay && sticky header

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,5 +1,6 @@
 const navMain = document.querySelector(".main-nav");
 const navToggle = document.querySelector(".toggle");
+const overlay = document.querySelector('.overlay');
 
 navMain.classList.remove("main-nav--nojs");
 navMain.classList.add("main-nav--closed");
@@ -8,4 +9,12 @@ navToggle.onclick = function() {
   navToggle.classList.toggle("toggle--active");
   navMain.classList.toggle('main-nav--closed');
   navMain.classList.toggle('main-nav--opened');
+  overlay.classList.toggle('overlay--reveal');
+};
+
+overlay.onclick = function() {
+  navToggle.classList.toggle("toggle--active");
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
+  overlay.classList.toggle('overlay--reveal');
 };
