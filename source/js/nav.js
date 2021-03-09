b1bcda466420013704cdf710afd8c206b5a70099
almax-21 <almaev21.max@gmail.com>
2021-03-09 06:57:06 +0400

    Refactoring nav.js

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,20 +1,21 @@
-const navMain = document.querySelector(".main-nav");
-const navToggle = document.querySelector(".toggle");
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.toggle');
 const navOverlay = document.querySelector('.overlay--nav');
 
-navMain.classList.remove("main-nav--nojs");
-navMain.classList.add("main-nav--closed");
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
 
-navToggle.onclick = function() {
-  navToggle.classList.toggle("toggle--active");
+const toggleNavClasses = () => {
+  navToggle.classList.toggle('toggle--active');
   navMain.classList.toggle('main-nav--closed');
   navMain.classList.toggle('main-nav--opened');
   navOverlay.classList.toggle('overlay--reveal');
 };
 
-navOverlay.onclick = function() {
-  navToggle.classList.toggle("toggle--active");
-  navMain.classList.toggle('main-nav--closed');
-  navMain.classList.toggle('main-nav--opened');
-  navOverlay.classList.toggle('overlay--reveal');
+navToggle.onclick = () => {
+  toggleNavClasses();
+};
+
+navOverlay.onclick = () => {
+  toggleNavClasses();
 };
